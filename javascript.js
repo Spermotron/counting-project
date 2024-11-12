d06af8c84b25b7539dfb84057f21a3faa07199f7
spermotron <spencerfermo17@gmail.com>
2024-11-11 20:44:21 -0800

    Created a rough draft of project
    Basically using this draft to test out deploying
    a website to the web.. I think. Idk we will see.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("titties are great")
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+let total = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    total += count;
+    saveEl.textContent = total;
+    count = 0;
+    countEl.textContent = count;
+}
